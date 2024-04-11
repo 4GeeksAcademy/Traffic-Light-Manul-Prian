c8213fcda41c234176075e56ec2014583f11daef
ManulPrian <156845683+ManulPrian@users.noreply.github.com>
2024-04-11 17:03:46 +0000

    este si que vale

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function TrafficLight() {
   const [color, setColor] = useState('red');
+  const [showPurple, setShowPurple] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -9,27 +10,44 @@ function TrafficLight() {
         setColor('yellow');
       } else if (color === 'yellow') {
         setColor('green');
+      } else if (color === 'green') {
+        setColor(showPurple ? 'purple' : 'red');
       } else {
         setColor('red');
       }
     }, 3000);
 
     return () => clearInterval(interval);
-  }, [color]);
+  }, [color, showPurple]);
 
   const handleClick = (clickedColor) => {
     setColor(clickedColor);
   };
 
+  const addPurple = () => {
+    setShowPurple(true);
+  };
+
+  const removePurple = () => {
+    setShowPurple(false);
+  };
+
   return (
-    <div className="traffic-light-container">
-      <div id="red" className={color === 'red' ? 'light red on' : 'light red'} onClick={() => handleClick('red')}></div>
-      <div id="yellow" className={color === 'yellow' ? 'light yellow on' : 'light yellow'} onClick={() => handleClick('yellow')}></div>
-      <div id="green" className={color === 'green' ? 'light green on' : 'light green'} onClick={() => handleClick('green')}></div>
+    <div>
+      <div className="traffic-light-container">
+        <div id="red" className={color === 'red' ? 'light red on' : 'light red'} onClick={() => handleClick('red')}></div>
+        <div id="yellow" className={color === 'yellow' ? 'light yellow on' : 'light yellow'} onClick={() => handleClick('yellow')}></div>
+        <div id="green" className={color === 'green' ? 'light green on' : 'light green'} onClick={() => handleClick('green')}></div>
+        {showPurple && <div id="purple" className={color === 'purple' ? 'light purple on' : 'light purple'} onClick={() => handleClick('purple')}></div>}
+      </div>
+      <button onClick={addPurple}>Que es esto?</button>
+      <button onClick={removePurple}>Nha, no me gusta</button>
     </div>
   );
 }
 
 export default TrafficLight;
+
+
 
 
